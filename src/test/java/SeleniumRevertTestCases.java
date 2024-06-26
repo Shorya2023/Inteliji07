dfad08be028d03c779673d975e2c4b36c4bda7f6
Shorya2023 <beohar77@gmail.com>
2024-06-26 20:29:46 +0530

    SeleniumRevertTestCases updated again with test acses addition

--- src/test/java/SeleniumRevertTestCases.java
+++ src/test/java/SeleniumRevertTestCases.java
@@ -19,7 +19,17 @@ public class SeleniumRevertTestCases {
     {
 
         System.out.println("Revert comiit success...............");
+        System.out.println("updated1...............");
+
     }
+
+    @Test(priority = 5,groups ="Regression")
+    public void AddedNow()
+    {
+
+        System.out.println("updated 2...............");
+    }
+
 
 
 
